df7ea9a04cd4b740f431de0fdadf468706ff6d9c
Alya <alyaramadhani000@gamil.com>
2021-03-17 18:29:21 +0700

    Soal 2

---
+++ Soal 2_Tugas 3.py
@@ -0,0 +1,15 @@
+Biodata = {"nama" : "Alya Ramadhani",
+           "hobi1" : "mendengarkan musik",
+           "hobi2" : "nonton film",
+           "hobi3" : "jalan-jalan",
+           "sosmed1" : "alyaaaaaaakk",
+           "sosmed2" : "aalyar",
+           "sosmed3" : "i_m_alyak",
+           "lagu_favorit1" : "dark_clouds",
+           "lagu_favorit2" : "orange",
+           "lagu_favorit3" : "don't_run_away",
+           "makanan_favorit1" : "coklat",
+           "makanan_favorit2" : "nasi goreng",
+           "makanan_favorit3" : "dimsum",
+           }
+print(Biodata["hobi2"])
