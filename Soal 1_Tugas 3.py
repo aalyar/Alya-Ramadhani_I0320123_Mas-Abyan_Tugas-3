bb74db440e018d6ef12e3339e702469e6fdbf27d
Alya <alyaramadhani000@gamil.com>
2021-03-17 17:49:22 +0700

    Extend list

--- Soal 1_Tugas 3.py
+++ Soal 1_Tugas 3.py
@@ -1,1 +1,10 @@
-list = ['Diva','Yuki', 'Rara', 'Salma','Ica','Aji']
+list = ['Diva','Yuki', 'Rara', 'Salma','Ica','Aji', 'Rahmat', 'Zafira', 'Tsania', 'Rana']
+print(list[4],list[6],list[7])
+
+list[3] = 'Wanda'
+list[5] = 'Utsman'
+list[9] = 'Rifqi'
+print(list)
+
+list.extend(['Maurich', 'Sekar'])
+print(list)
